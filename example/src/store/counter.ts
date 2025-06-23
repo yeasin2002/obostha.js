21efc32ab2127bb45d8676398c901f9263957837
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-06-24 03:40:22 +0600

    chore: added example  counter  with react

---
+++ example/src/store/counter.ts
@@ -0,0 +1,10 @@
+import { create } from '../../../src'
+
+type CounterState = { count: number }
+
+export const useCounterStore = create<CounterState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  reset: () => set({ count: 0 }),
+}))
